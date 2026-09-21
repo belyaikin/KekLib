6fca2fc03fa6c5d966e2443e8912b6addddf37b5
belyaikin <dmitriybelyaikin@gmail.com>
2026-09-21 18:30:09 +0500

    Add KekScene subproject

---
+++ KekScene/include/KekScene/Scene.h
@@ -0,0 +1,11 @@
+//
+// Created by Dmitriy on 21.09.2026.
+//
+
+#pragma once
+
+namespace Kek::Scene {
+    class Scene {
+
+    };
+}
---
+++ KekScene/src/Scene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dmitriy on 21.09.2026.
+//
+
+#include
